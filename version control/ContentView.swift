741fd158cb8249cbdac3bbe38a9b3b1f9305b12c
BT_C09_02 <mobileappdev@bths.edu>
2026-04-20 08:58:15 -0400

    Initial Commit

---
+++ version control/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  version control
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ version control/version_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  version_controlApp.swift
+//  version control
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct version_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
