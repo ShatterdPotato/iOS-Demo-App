86ea969c420385eed8f942fcce0245874346b309
BT_C09_02 <mobileappdev@bths.edu>
2026-04-20 09:05:08 -0400

    Modified SampleView to use ContentView and removed text from ContentView

--- version control/SampleView.swift
+++ version control/SampleView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct SampleView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ContentView()
+            Text("Invoked ContentView()")
+        }
     }
 }
 
