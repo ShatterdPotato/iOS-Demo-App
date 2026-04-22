e33d294d6d82d9f06fdf7475cb05ea1f9c4d7e85
BT_C09_02 <mobileappdev@bths.edu>
2026-04-22 09:27:10 -0400

    Added Constatns struct with some constants

--- version control/Constants.swift
+++ version control/Constants.swift
@@ -4,4 +4,10 @@
 //
 //  Created by Student on 4/20/26.
 //
+import Foundation
+import SwiftUI
 
+struct Constants {
+    public static let favoriteColor = Color.green
+    public static let pecialNumber = 18
+}
